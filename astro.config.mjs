0d85276ffaaf64dba87e4b71ef230a8b30caf6dc
John <69305733+Konstantine1307@users.noreply.github.com>
2024-03-02 10:52:18 +0000

    added site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://konstantine1307.github.io',
   integrations: [tailwind({
     nesting: true
   })]
